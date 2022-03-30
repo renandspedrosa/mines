8f34af5597c1e388037c3ee4a02287f0f61df37f
Renan Pedrosa <renandspedrosa@gmail.com>
2022-03-30 20:47:02 -0300

    field,mine,label

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import params from './src/params';
+import Field from './src/components/Field';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <StatusBar style="auto" />
       
       <Text>Iniciando o Mines!</Text>
       <Text>Tamanho da grade: {params.getRowsAmount()} X {params.getColumnsAmount()} </Text>
-      <StatusBar style="auto" />
+      <Field/>
+      <Field opened/>
+      <Field opened  nearMines={1}/>
+      <Field opened  nearMines={2}/>
+      <Field opened  nearMines={3}/>
+      <Field opened  nearMines={6}/>
+      <Field mined />
+      <Field mined opened />
+      <Field mined opened exploded/>
     </View>
   );
 }
