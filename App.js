908b923dce7322f90661f220b850de44422fccd3
Renan Pedrosa <renandspedrosa@gmail.com>
2022-04-04 23:10:43 -0300

    final

--- App.js
+++ App.js
@@ -1,38 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import params from './src/params';
-import Field from './src/components/Field';
+import MineField from './src/components/MineField';
+import { createMinedBoard } from './src/functions';
+
 
 export default function App() {
+  
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+  
+  createState = () =>{
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return {
+      board : createMinedBoard(rows, cols, minesAmount()),
+    }
+  }
+  
+  const [states, setstates] = useState(createState())
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       
       <Text>Iniciando o Mines!</Text>
       <Text>Tamanho da grade: {params.getRowsAmount()} X {params.getColumnsAmount()} </Text>
-
-
-      <Field/>
-      <Field opened/>
-      <Field opened  nearMines={1}/>
-      <Field opened  nearMines={2}/>
-      <Field opened  nearMines={3}/>
-      <Field opened  nearMines={6}/>
-      <Field mined />
-      <Field mined opened />
-      <Field mined opened exploded/>
-      <Field flagged/>
-      <Field flagged opened/>
+      <View style={styles.board}>
+        <MineField board={states.board} />
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#F5FCFF'
+    justifyContent: 'flex-end',
   },
+  board:{
+    alignItems:'center',
+    backgroundColor:'#AAA'
+  }
 });
